bed715ac5e5c3cfe84f158972efa2f7718c3bc16
Nisaruj <nisaruj@hotmail.com>
2019-04-19 09:15:42 +0700

    Add static path

--- server.js
+++ server.js
@@ -1,26 +1,39 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mathLib = require('mathjs');
+const rp =  require('request-promise');
 const app = express()
 const port = process.env.PORT || 8081
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json({type: 'application/*+json'}));
 app.use(bodyParser.json());
+app.use(express.static('public'));
+
+const weatherConfig = {
+  appID: "25370c1dcd88f560ee7021ead58a657c"
+}
 
 app.get('/', (req, res) => {
   res.send('Hello world');
 })
 
+function getWeather() {
+  return rp(`https://api.openweathermap.org/data/2.5/weather?q=London&APPID=${weatherConfig.appID}`);
+}
+
 app.post('/webhook', (req, res) => {
   res.setHeader('Content-Type', 'application/json');
-
+  
   let output;
 
   if (req.body.queryResult.intent.displayName == "Calculator") {
     output = {'fulfillmentText': mathLib.eval(req.body.queryResult.parameters.expression)}
   } else if (req.body.queryResult.intent.displayName == "Time Telling") {
-    output = {'fulfillmentText': 'ขณะนี้เวลา' + new Date(Date.now()).toLocaleString('en-GB', { timeZone: "Asia/Bangkok"})}
+    output = {'fulfillmentText': 'ขณะนี้เวลา ' + new Date(Date.now()).toLocaleString('en-GB', { timeZone: "Asia/Bangkok"})}
+  } else if (req.body.queryResult.intent.displayName == "Weather") {
+    //console.log(req.body.queryResult.parameters);
+    //output = {'fulfillmentText': ''};
   } else {
     output = {'fulfillmentText': "What are you talking about?"}
   }
